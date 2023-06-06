e533ac68844437c4d80277aa08d5c7e1517aaa45
xxai.art <xxai.art@gmail.com>
2023-06-06 16:33:03 +0800

    ♨

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # awp
+
+wrap axum for easy use
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#![feature(impl_trait_in_assoc_type)]
+pub mod env;
+pub mod err;
+pub mod log;
+pub mod srv;
